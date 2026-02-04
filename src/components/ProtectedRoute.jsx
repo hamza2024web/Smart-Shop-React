d8267d119a781dff72c167609d907dab528576d1
hamza2024web <hamzaakroubi1528@gmail.com>
2026-02-04 11:54:29 +0100

    feat : create the protected route component to check if the user is logged before showing him a page

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import {useAuth} from "../context/AuthContext.jsx";
+import {Navigate} from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    if (loading) return <div>Loading...</div>;
+    if (!user) return <Navigate to="/login" replace />;
+    return children;
+};
+export default ProtectedRoute;
